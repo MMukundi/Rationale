5bbb7b47f1f933f421eb21041c6736bd3761e6d0
MMukundi <musicman5978@gmail.com>
2020-03-17 10:08:51 -0700

    edited tsconfig

--- built/comprehend.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Comprehend;
-(function (Comprehend) {
-    function isTrue(b) {
-        return b;
-    }
-    Comprehend.isTrue = isTrue;
-})(Comprehend = exports.Comprehend || (exports.Comprehend = {}));
--- built/compute.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Compute;
-(function (Compute) {
-    function add(a, b) {
-        return a + b;
-    }
-    Compute.add = add;
-})(Compute = exports.Compute || (exports.Compute = {}));
